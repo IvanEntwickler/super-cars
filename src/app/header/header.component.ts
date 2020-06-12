492299fe9ee1942bce82c76fc41b22d58f9fbaea
IvanEntwickler <53708215+IvanEntwickler@users.noreply.github.com>
2020-06-12 21:34:48 +0200

    finished sidebar and  applied logic for routing between home and models page via the sidebar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,13 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   showSidebar = false;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   onMenuClick() {
     this.showSidebar = !this.showSidebar;
+  }
+
+  onNavigateHome() {
+    this.router.navigate(['/home']);
   }
 
 }
