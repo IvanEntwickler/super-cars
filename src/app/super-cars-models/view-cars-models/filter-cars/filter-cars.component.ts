9a9e671f79a041cf522ec6fca6179257e8b3233d
IvanEntwickler <53708215+IvanEntwickler@users.noreply.github.com>
2020-06-27 13:08:53 +0200

    worked on filterComponent for more functional checkboxes

--- src/app/super-cars-models/view-cars-models/filter-cars/filter-cars.component.ts
+++ src/app/super-cars-models/view-cars-models/filter-cars/filter-cars.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-filter-cars',
@@ -9,9 +9,21 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class FilterCarsComponent implements OnInit {
   filterForm: FormGroup;
   modelArray = [
-    'e-tron', 'A1', 'A3', 'A4',
-    'A5', 'A6', 'A7', 'A8',
-    'Q2', 'Q5', 'Q7', 'Q8', 'TT', 'R8', 'RS'
+    {name: 'e-tron', value: 'e-tron'},
+    {name: 'A1', value: 'A1'},
+    {name: 'A3', value: 'A3'},
+    {name: 'A4', value: 'A4'},
+    {name: 'A5', value: 'A5'},
+    {name: 'A6', value: 'A6'},
+    {name: 'A7', value: 'A7'},
+    {name: 'A8', value: 'A8'},
+    {name: 'Q2', value: 'Q2'},
+    {name: 'Q5', value: 'Q5'},
+    {name: 'Q7', value: 'Q7'},
+    {name: 'Q8', value: 'Q8'},
+    {name: 'TT', value: 'TT'},
+    {name: 'R8', value: 'R8'},
+    {name: 'RS', value: 'RS'}
   ];
   vehicleArray = [
     'Alternative Antriebe', 'g-tron', 'Plug-in-Antriebe',
@@ -25,10 +37,14 @@ export class FilterCarsComponent implements OnInit {
 /// formControls for each category
   ngOnInit() {
     this.filterForm = new FormGroup({
-      model: new FormControl(),
+      modelArr: new FormArray([]),
       vehicle: new FormControl(),
       price: new FormControl('25000')
     });
+  }
+
+  getControls() {
+    return (this.filterForm.get('modelArr') as FormArray).controls;
   }
 
   /// submiting the form and reseting it to the initial values
