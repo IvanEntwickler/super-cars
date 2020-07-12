5b6b39bb9993cb6b754419b8f7834ae91ea4180b
IvanEntwickler <53708215+IvanEntwickler@users.noreply.github.com>
2020-07-12 20:29:11 +0200

    added the summary-view component

--- src/app/car-configuration/summary-view/summary-view.component.ts
+++ src/app/car-configuration/summary-view/summary-view.component.ts
@@ -1,18 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CarModelService } from './../../car-model.service';
+import { Car } from './../../interface';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-summary-view',
   templateUrl: './summary-view.component.html',
   styleUrls: ['./summary-view.component.scss']
 })
-export class SummaryViewComponent implements OnInit {
+export class SummaryViewComponent implements OnInit, OnDestroy {
+  extras = 0; // for the moment later dynamic
+  car: Car;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private carModelService: CarModelService) { }
 
   ngOnInit(): void {
+    this.subscription = this.carModelService.getCarState()
+    .subscribe(car => this.car = car);
   }
   /// 3.0% von Basis auf advanced im Durchschnitt
   /// 8.0% von Basis auf s line im Durchschnitt
   /// 15% von Basis auf edition one im Durchschnitt
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
